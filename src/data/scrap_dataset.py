d909364ab1c273c521d0297fbb065c7ac4af65cf
Renan Costa Alencar <renan.costaalencar@gmail.com>
2023-04-10 21:35:44 -0300

    Fixed missing tags from tags

--- src/data/scrap_dataset.py
+++ src/data/scrap_dataset.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Reference: https://jovian.com/mihirpanchal0072/web-scraping-imdb-database-for-movies-using-python-beautiful-soup
 import os
+import logging
 import re
 import json
 import string
@@ -16,6 +17,14 @@ GENRE_LIST = ['action', 'adventure', 'animation', 'biography', 'comedy', 'crime'
 
 '''List of popular TV parental ratings'''
 TV_PARENTAL_RATING = ['TV-Y', 'TV-Y7', 'TV-G', 'TV-PG', 'TV-14', 'TV-MA']
+
+'''List of missing values'''
+MISSING_VALUES = ['not certified', 'post-production', 'filming', 'announced']
+
+'''Maximum number of pages per title type'''
+MAX_PAGES = 199
+
+LOGGER = logging.getLogger(__name__)
 
 '''Function to get the page content of the topic'''
 def get_topics_page(genre, page_number=None):
@@ -41,30 +50,34 @@ def get_topics_page(genre, page_number=None):
     return doc
 
 '''Function to get the movie uid from the page content'''
-def get_uid(doc):
-    uid_selector="loadlate"            
-    movie_uid_tags=doc.find_all('img',{'class':uid_selector})
-    movie_uid=[]
+def get_movie_uid(doc):
+    uid_selector = "loadlate"            
+    movie_uid_tags = doc.find_all('img',{'class':uid_selector})
+    movie_uid = []
+
     for tag in movie_uid_tags:
         uid = tag.attrs['data-tconst']
         movie_uid.append(uid)
+
     return movie_uid
 
 '''Function to get the movie rank from the page content'''
-def get_rank(doc):
-    rank_selector="lister-item-index unbold text-primary"            
-    movie_rank_tags=doc.find_all('span',{'class':rank_selector})
-    movie_rank=[]
+def get_movie_rank(doc):
+    rank_selector = "lister-item-index unbold text-primary"            
+    movie_rank_tags = doc.find_all('span',{'class':rank_selector})
+    movie_rank = []
+
     for tag in movie_rank_tags:
         rank = tag.get_text().strip('.')
         movie_rank.append(rank)
+
     return movie_rank
 
 '''Function to get the movie name from the page content'''
 def get_movie_name(doc):
-    selection_class="lister-item-header"
-    movie_name_tags=doc.find_all('h3',{'class':selection_class})
-    movie_name=[]
+    selection_class = "lister-item-header"
+    movie_name_tags = doc.find_all('h3',{'class':selection_class})
+    movie_name = []
 
     for tag in movie_name_tags:
         name = tag.find('a').text
@@ -75,44 +88,67 @@ def get_movie_name(doc):
 '''Function to get the movie year from the page content'''
 def get_movie_year(doc):
     year_selector = "lister-item-year text-muted unbold"           
-    movie_year_tags=doc.find_all('span',{'class':year_selector})
-    movie_year=[]
+    movie_year_tags = doc.find_all('span',{'class':year_selector})
+    movie_year = []
+
     for tag in movie_year_tags:
-        movie_year.append(tag.get_text().strip("()"))
+        year = 0
+        try:
+            # get only the year from the text
+            year = int(re.search(r'\d+', tag.get_text()).group())
+        except:
+            LOGGER.error('year not found')
+
+        movie_year.append(year)
+
     return movie_year
 
 '''Function to get the movie certificate from the page content'''
-def get_certificate(doc):
-    
-    selection_class="certificate"
-    movie_certificate_tags=doc.find_all('span',{'class':selection_class})
-    movie_certificate=[]
+def get_movie_certificate(doc):
+    # get the first sibling p.text-muted of the h3.lister-item-header 
+    movie_feature_tags = doc.select('h3.lister-item-header + p.text-muted')
+    movie_certificate = []
 
-    for tag in movie_certificate_tags:
-        certificate = tag.text[:10]
+    for tag in movie_feature_tags:
+        certificate = 'not certified'
+        # get the span.certificate right inside the p.text-muted
+        content = tag.select_one('p.text-muted > span.certificate')
+        
+        # check if it is in post-production
+        production = tag.select_one('p.text-muted > span.ghost + b')
+        if production is not None:
+            certificate = production.get_text().lower()
+        # save the certificate if it is not missing
+        if content is not None:
+            certificate = content.text[:10]
+
         movie_certificate.append(certificate)
     
     return movie_certificate
 
-'''Function to get the movie duration from the page content'''
-def get_duration(doc):
+
+'''Function to get the movie runtime from the page content'''
+def get_movie_runtime(doc):
+    # get the first sibling p.text-muted of the h3.lister-item-header 
+    movie_feature_tags = doc.select('h3.lister-item-header + p.text-muted')
+    movie_runtime = []
+
+    for tag in movie_feature_tags:
+        runtime = 0
+        # get the span.runtime right inside the p.text-muted
+        content = tag.select_one('p.text-muted > span.runtime')
+
+        if content is not None:
+            runtime = content.text[:10].replace(' min','')
+        movie_runtime.append(runtime)
     
-    selection_class = "runtime"
-    movie_duration_tags=doc.find_all('span',{'class':selection_class})
-    movie_duration=[]
-    
-    for tag in movie_duration_tags:
-        duration = tag.text[:10].replace(' min','')
-        movie_duration.append(int(duration))
-    
-    return movie_duration
+    return movie_runtime
 
 '''Function to get the movie genre from the page content'''
-def get_genre(doc):
-    
+def get_movie_genre(doc):
     selection_class = "genre"
-    movie_genre_tags=doc.find_all('span',{'class':selection_class})
-    movie_genre=[]
+    movie_genre_tags = doc.find_all('span',{'class':selection_class})
+    movie_genre = []
     
     for tag in movie_genre_tags:
         genre = re.sub('['+string.punctuation+']', '', tag.get_text().lower()).split()
@@ -121,41 +157,46 @@ def get_genre(doc):
     return movie_genre
 
 '''Function to get the movie rating from the page content'''
-def get_rating(doc):
-    
-    rating_selector="inline-block ratings-imdb-rating"            
-    movie_rating_tags=doc.find_all('div',{'class':rating_selector})
-    movie_rating=[]
-    for tag in movie_rating_tags:
-        rating = float(tag.get_text().strip())
-        movie_rating.append(rating)
+def get_movie_rating(doc):
+    movie_feature_tags = doc.select('div.lister-item-content')
+    movie_rating = []
 
+    for tag in movie_feature_tags:
+        rating = 0.0
+
+        # get the div.ratings-bar right after the p.text-muted
+        content = tag.select_one('p.text-muted + div.ratings-bar')
+        if content is not None:
+            rating = float(content.get_text().strip().split('\n')[0])
+
+        movie_rating.append(rating)
+    
     return movie_rating
 
 '''Function to get the movie director from the page content'''
-def get_director(doc):
-    
+def get_movie_director(doc):
     selection_class = ""
-    movie_director_tags=doc.find_all('p',{'class':selection_class})
-    movie_director=[]
+    movie_director_tags = doc.find_all('p',{'class':selection_class})
+    movie_director = []
     
     for tag in movie_director_tags:
-        director = tag.find('a').text
-        if not "|" in tag.text:
-            director = ""    
+        director = ""
+        if "|" in tag.text:
+            director = tag.find('a').text
+
         movie_director.append(director)
     
     return movie_director
 
 '''Function to get the movie stars from the page content'''
-def get_stars(doc):
-    
+def get_movie_stars(doc):
     selection_class = ""
-    movie_stars_tags=doc.find_all('p',{'class':selection_class})
-    movie_stars=[]
+    movie_stars_tags = doc.find_all('p',{'class':selection_class})
+    movie_stars = []
     
     for tag in movie_stars_tags:
         stars = []
+        # get all links limited to top 5
         links = tag.find_all('a', limit=5)
         
         ghost_tag = tag.find('span',{'class':'ghost'})
@@ -168,22 +209,26 @@ def get_stars(doc):
     return movie_stars
 
 '''Function to get the movie number of votes from the page content'''
-def get_num_votes(doc):
-    
-    votes_selector="sort-num_votes-visible"            
-    movie_votes_tags=doc.find_all('p',{'class':votes_selector})
-    movie_votes=[]
+def get_movie_num_votes(doc):
+    movie_feature_tags = doc.select('div.lister-item-content')
+    movie_votes = []
 
-    for tag in movie_votes_tags:
+    for tag in movie_feature_tags:
         num_votes = 0
-        if tag.find('span',{'name':'nv'}) is not None:
-            num_votes = tag.find('span',{'name':'nv'}).attrs['data-value']
-        movie_votes.append(int(num_votes))
 
+        # get the span[name="nv"] right after the div.ratings-bar
+        content = tag.select_one('p.sort-num_votes-visible > span[name="nv"]')
+        if content is not None:
+            num_votes = content.attrs['data-value']
+
+        movie_votes.append(num_votes)
+    
     return movie_votes
 
 '''Function to get the movie data from the page content'''
-def imdb_dict():
+def imdb_dict(genre_search, num_pages=1):
+    LOGGER.info('scraping IMDB movies for \'%s\'...' % genre_search)
+
     # Let's we create a dictionary to store data of all movies
     movies_dictionary={
         'uid':[],
@@ -191,7 +236,7 @@ def imdb_dict():
         'name':[],
         'year':[],
         'certificate':[],
-        'duration':[],
+        'runtime':[],
         'genre':[],
         'rating':[],
         'director':[],
@@ -200,38 +245,28 @@ def imdb_dict():
     }
 
     # We have to scrap more than one page so we want urls of all pages with the help of loop we can get all urls
-    for i in range(1,2000,50):
+    # each page has 50 movies so we have to get 50 movies from each page (1~51)
+    num_titles = (num_pages * 50) + 2
+    for i in range(51, num_titles, 50):
+        LOGGER.info('[genre: \'%s\', page: %d/%d]' % (genre_search, i/50, num_titles/50))
         # Parse using BeautifulSoup
         try:
-            doc = get_topics_page('crime')
+            doc = get_topics_page(genre_search, i)
         except Exception as e:
-            print(e)
+            LOGGER.error(e)
             break
 
-        uid = get_uid(doc)
-        # print(len(uid))
-        rank = get_rank(doc)
-        # print(len(rank))
+        uid = get_movie_uid(doc)
+        rank = get_movie_rank(doc)
         name = get_movie_name(doc)
-        # print(len(name))
         year = get_movie_year(doc)
-        # print(len(year))
-        certificate = get_certificate(doc)
-        # print(certificate)
-        # print(len(certificate))
-        duration = get_duration(doc)
-        # print(duration)
-        # print(len(duration))
-        genre = get_genre(doc)
-        # print(len(genre))
-        rating = get_rating(doc)
-        # print(len(rating))
-        director = get_director(doc)
-        # print(len(director))
-        stars = get_stars(doc)
-        # print(len(stars))
-        num_votes = get_num_votes(doc)
-        # print(len(num_votes))
+        certificate = get_movie_certificate(doc)
+        runtime = get_movie_runtime(doc)
+        genre = get_movie_genre(doc)
+        rating = get_movie_rating(doc)
+        director = get_movie_director(doc)
+        stars = get_movie_stars(doc)
+        num_votes = get_movie_num_votes(doc)
 
         # We are adding every movie data to dictionary
         for i in range(len(name)):
@@ -240,7 +275,7 @@ def imdb_dict():
             movies_dictionary['name'].append(name[i])
             movies_dictionary['year'].append(year[i])
             movies_dictionary['certificate'].append(certificate[i])
-            movies_dictionary['duration'].append(duration[i])
+            movies_dictionary['runtime'].append(runtime[i])
             movies_dictionary['genre'].append(genre[i])
             movies_dictionary['rating'].append(rating[i])
             movies_dictionary['director'].append(director[i])
@@ -249,30 +284,40 @@ def imdb_dict():
         
     return pd.DataFrame(movies_dictionary)
 
-'''Function to remove duplicate movies from the dataframe'''
+'''Function to remove duplicate movies from the data-frame'''
 def remove_duplicates(df):
+    LOGGER.info('removing duplacate data...')
+
     # We are removing duplicate movies
     df.drop_duplicates(subset='uid', keep='first', inplace=True)
     return df
 
 '''Function to clean the data'''
 def clean_data(df):
+    LOGGER.info('Cleaning data...')
+
     # We are cleaning data
     df = remove_duplicates(df)
+    df['rank'] = df['rank'].astype(int)
     df['year'] = df['year'].astype(int)
-    df['duration'] = df['duration'].astype(int)
+    df['runtime'] = df['runtime'].astype(int)
     df['rating'] = df['rating'].astype(float)
     df['num_votes'] = df['num_votes'].astype(int)
     return df
 
-'''Function to save only movies in the dataframe'''
+'''Function to save only movies in the data-frame'''
 def save_only_movies(df):
+    LOGGER.info('Saving only movies onto database...')
+
     # We are saving only movies
     df = df[~df['certificate'].isin(TV_PARENTAL_RATING)]
+    df = df[~df['certificate'].isin(MISSING_VALUES)]
     return df
 
 '''Function to save data to database'''
 def save_to_db(df):
+    LOGGER.info('saving data to database...')
+
     # We are saving data to database
     json_obj = json.loads(df.to_json(orient='records'))
     # Making a Connection with MongoClient
@@ -283,61 +328,57 @@ def save_to_db(df):
     movies = db["movies"]
     # creating index
     movies.create_index([('uid', ASCENDING)], unique=True)
-
+    
+    if json_obj == []:
+        LOGGER.info('no movies to insert')
+        return
+    
+    num_insertions = 0
     try:
-        movie_id = movies.insert_many(json_obj)
+        result = movies.insert_many(json_obj, ordered=False).inserted_ids
+        num_insertions = len(result)
     except errors.BulkWriteError as bwe:
-        print(bwe.details)
+        write_errors = bwe.details['writeErrors']
+        for we in write_errors:
+            LOGGER.error(we['errmsg'])
+        num_insertions = bwe.details['nInserted']
     finally:
-        print(movie_id)
+        client.close()
+        LOGGER.info('%d movies inserted' % num_insertions)
 
 '''Function to carry out a single page test'''
 def single_page_test():
-    doc = get_topics_page('crime')
-    uid = get_uid(doc)
-    rank = get_rank(doc)
-    name = get_movie_name(doc)
-    year = get_movie_year(doc)
-    certificate = get_certificate(doc)
-    duration = get_duration(doc)
-    genre = get_genre(doc)
-    rating = get_rating(doc)
-    director = get_director(doc)
-    stars = get_stars(doc)
-    votes = get_num_votes(doc)
+    LOGGER.info('starting single test page.')
 
-    movie_dictionary={
-        'uid':uid,
-        'rank':rank,
-        'name':name,
-        'year':year,
-        'certificate':certificate,
-        'duration':duration,
-        'genre':genre,
-        'rating':rating,
-        'director':director,
-        'stars':stars,
-        'num_votes':votes,
-    }
+    for genre in GENRE_LIST:
+        df = imdb_dict(genre)
 
-    df = pd.DataFrame.from_dict(movie_dictionary,orient='index')
-    df = df.transpose()
-    print(df)
+        df = save_only_movies(df)
+        df = clean_data(df)
 
-    save_to_db(df)
+        save_to_db(df)
 
 '''Function to carry out the main program'''
 def main():
-    df = imdb_dict()
-    df = clean_data(df)
-    df = save_only_movies(df)
-    save_to_db(df)
+    """ Runs data processing scripts to turn raw data from (../raw) into
+        cleaned data ready to be analyzed (saved in ../processed).
+    """
+    LOGGER.info('getting data from IMDB website.')
+    
+    for genre in GENRE_LIST:
+        df = imdb_dict(genre, MAX_PAGES)
+        df = save_only_movies(df)
+        df = clean_data(df)
+
+        save_to_db(df)
 
 if __name__ == "__main__":
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
 
-    single_page_test()
-    # main()
+    main()
+    # single_page_test()
